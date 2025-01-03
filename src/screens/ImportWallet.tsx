ca55e63d6318c8323690047285c9ed62a5f567db
Utkarsh4517 <uttkarsh.dav@gmail.com>
2025-01-04 02:03:06 +0530

    fix: design of import wallet page

--- src/screens/ImportWallet.tsx
+++ src/screens/ImportWallet.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, TextInput, Alert} from 'react-native';
+import {View, Text, TextInput, Alert, Image} from 'react-native';
 import {Button} from '../components/Button';
 import {Keypair} from '@solana/web3.js';
 // import RNSecureStorage, { ACCESSIBLE } from 'rn-secure-storage';
@@ -11,15 +11,17 @@ export const ImportWallet = () => {
 
   const importWallet = async () => {
     try {
-      const secretKey = new Uint8Array(privateKey.split(',').map(num => parseInt(num)));
+      const secretKey = new Uint8Array(
+        privateKey.split(',').map(num => parseInt(num)),
+      );
       const keypair = Keypair.fromSecretKey(secretKey);
-      
+
       // Store private key securely
-    //   await RNSecureStorage.setItem(
-    //     'walletPrivateKey',
-    //     JSON.stringify(Array.from(keypair.secretKey)),
-    //     {accessible: ACCESSIBLE.WHEN_UNLOCKED}
-    //   );
+      //   await RNSecureStorage.setItem(
+      //     'walletPrivateKey',
+      //     JSON.stringify(Array.from(keypair.secretKey)),
+      //     {accessible: ACCESSIBLE.WHEN_UNLOCKED}
+      //   );
 
       navigation.navigate('Home' as never);
     } catch (error) {
@@ -29,25 +31,37 @@ export const ImportWallet = () => {
 
   return (
     <View className="flex-1 bg-[#E9FFE4] p-4">
-      <View className="flex-1 justify-center">
-        <Text className="text-2xl font-bold text-[#3E3E3E] mb-2">
+      <View className="flex-1 items-center">
+        <Text className="text-2xl font-bold text-[#3E3E3E] mb-2 mt-20">
           Import Your Wallet
         </Text>
         <Text className="text-base text-[#3E3E3E] mb-6">
           Enter your wallet's private key to import it
         </Text>
-        
+        <View className="flex-1"></View>
+        <Image
+          source={require('../assets/images/cat3.png')}
+          className="w-72 h-72 mt-28 ml-6"
+          resizeMode="contain"></Image>
+
         <TextInput
-          className="bg-white p-4 rounded-lg mb-6 text-[#3E3E3E]"
-          placeholder="Enter private key"
+          className="bg-[#3E3E3E] p-4 rounded-xl mb-6 text-white w-full h-20 text-center text-base font-bold"
+          placeholder="Enter your private key"
+          placeholderTextColor="white"
           value={privateKey}
           onChangeText={setPrivateKey}
           multiline
           numberOfLines={4}
+          textAlignVertical="center"
+          style={{
+            textAlign: 'center',
+            verticalAlign: 'middle',
+          }}
         />
 
         <Button onPress={importWallet} title="Import Wallet" />
+        <View className="h-4"></View>
       </View>
     </View>
   );
-}; 
+};
